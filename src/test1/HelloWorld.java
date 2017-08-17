76d9e8826ac7026052792dc58661c1f2b9a4f181
Chosooyoung <Chosooyoung@218.38.137.28>
2017-08-17 23:11:56 +0900

    second commit

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -3,6 +3,6 @@ package test1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Window");
-		System.out.println("hhhhhhh");
+		System.out.println("Hi World");
 	}
 }
